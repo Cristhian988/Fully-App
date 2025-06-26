81ccc1bfdc21507342bee33937d0ceca6b143773
Criss <crissfx28@gmail.com>
2025-06-26 11:44:26 -0500

    created a Camp component

--- src/components/Camp.tsx
+++ src/components/Camp.tsx
@@ -1,7 +1,106 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { PEOPLE_URL } from "@/constants";
+
+interface CampSiteProps {
+  backgroundImage: string;
+  title: string;
+  subtitle: string;
+  peopleJoined: string;
+}
 
 export default function Camp() {
+  const handleScroll = (e: React.WheelEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.currentTarget.scrollLeft += e.deltaY;
+  };
+
   return (
-    <div>Camp</div>
-  )
+    <section className="xl:max-w-[1440px] mx-auto relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]" onWheel={handleScroll}>
+        <CampSite
+          backgroundImage="bg-img-1"
+          title="Putuk Truno Camp"
+          subtitle="Prigen, Pasuruan"
+          peopleJoined="50+ Joined"
+        />
+        <CampSite
+          backgroundImage="bg-img-2"
+          title="Mountain View Camp"
+          subtitle="Somewhere in the Wilderness"
+          peopleJoined="50+ Joined"
+        />
+      </div>
+
+      <div className="flex items-center justify-end mt-10 px-6 lg:-mt-60 lg:mr-6">
+        <div className="bg-green-600 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-4xl xl:px-12 xl:py-16 relative w-full overflow-hidden rounded-3xl">
+          <h2 className="text-2xl md:text-4xl xl:text-6xl capitalize text-white">
+            <strong>Feeling Lost</strong> And Not Knowing The Way?
+          </h2>
+          <p className="text-base md:text-lg xl:text-xl mt-5 text-white">
+            Starting from the anxiety of the climbers when visiting a new
+            climbing location, the possibility of getting lost is very large.
+            That&apos;s why we are here for those of you who want to start an
+            adventure
+          </p>
+          <Image
+            src="/quote.svg"
+            alt="camp-2"
+            width={186}
+            height={219}
+            className="camp-quote"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
+
+export const CampSite = ({
+  backgroundImage,
+  title,
+  subtitle,
+  peopleJoined,
+}: CampSiteProps) => {
+  return (
+    <div
+      className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-3xl xl:rounded-4xl`}
+    >
+      <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
+        <div className="flex items-center justify-center gap-4">
+          <div className="rounded-full bg-green-600 p-4">
+            <Image src="/folded-map.svg" alt="map" width={28} height={28} />
+          </div>
+          <div className="flex flex-col gap-1">
+            <h4 className="text-base md:text-lg xl:text-xl text-white">
+              {title}
+            </h4>
+            <p className="text-base md:text-lg xl:text-xl text-white">
+              {subtitle}
+            </p>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center gap-6">
+          <span className="flex -space-x-4 overflow-hidden">
+            {PEOPLE_URL.map((url) => (
+              <Image
+                className="inline-block h-10 w-10 rounded-full"
+                src={url}
+                key={url}
+                alt="person"
+                width={52}
+                height={52}
+              />
+            ))}
+          </span>
+          <p className="text-base md:text-lg xl:text-xl text-white">
+            {peopleJoined}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
